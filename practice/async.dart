eb882610d36f35a341994e73c5283d850d04f9fa
Huy Phúc <thienxa2820022@gmail.com>
2023-02-15 22:30:24 +0700

    Day 1 and 2

---
+++ practice/async.dart
@@ -0,0 +1,22 @@
+// import '../dart/async.dart';
+
+// String addHello(String name) {
+//   return 'Hello $name';
+// }
+
+// Future<String> greetUser() async {
+//   var username = await fetchUsername();
+//   return addHello(username);
+// }
+
+// Future<String> sayGoodbye() async {
+//   try {
+//     const result = await logoutUser();
+//     return '$result Thanks, see you next time';
+//   } catch (e) {
+//     return 'Bye bye';
+//   }
+// }
+
+
+
